99255cafd48a9ddc208ac0bf9292b664ed7cdb4b
zdy <zdy@P3-01171.p3-group.com>
2019-01-03 14:48:03 +0100

    initial check-in of the FreshVotes2 code

---
+++ FreshVotes2/src/main/java/com/freshvotes2/FreshVotes2Application.java
@@ -0,0 +1,14 @@
+package com.freshvotes2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshVotes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshVotes2Application.class, args);
+	}
+
+}
+
